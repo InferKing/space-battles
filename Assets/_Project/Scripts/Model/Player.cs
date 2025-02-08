de7232693f7396d5cfb160291fa6ce75c8da18b0
Oleg <54029296+InferKing@users.noreply.github.com>
2025-02-08 13:48:34 +0500

    start to add player

--- Assets/_Project/Scripts/Model/Player.cs
+++ Assets/_Project/Scripts/Model/Player.cs
@@ -1,7 +1,18 @@
-﻿namespace _Project.Scripts.Model
+﻿using _Project.Scripts.Configs;
+
+namespace _Project.Scripts.Model
 {
     public class Player
     {
+        private PlayerData _playerData;
         
+        public Player(PlayerData playerData)
+        {
+            // TODO: что-то должно подаваться, что влияет на изначальные уровни апгрейдов
+            _playerData = playerData;
+            LevelSystem = new XpLevelSystem();
+        }
+        
+        public XpLevelSystem LevelSystem { get; }
     }
 }
---
+++ Assets/_Project/Scripts/Model/XPLevelSystem.cs
@@ -0,0 +1,32 @@
+﻿using UniRx;
+
+namespace _Project.Scripts.Model
+{
+    public class XpLevelSystem
+    {
+        public ReactiveProperty<int> Level { get; private set; }
+        public ReactiveProperty<int> CurrentExp { get; private set; }
+        public int ExpToLevelUp { get; private set; } = 10;
+        public int TotalExp { get; private set; } = 0;
+
+        public XpLevelSystem()
+        {
+            Level = new ReactiveProperty<int>(1);
+            CurrentExp = new ReactiveProperty<int>(0);
+        }
+        
+        public void AddExp(int exp)
+        {
+            if (exp <= 0) return;
+            
+            TotalExp += exp;
+            CurrentExp.Value += exp;
+
+            if (CurrentExp.Value < ExpToLevelUp) return;
+            
+            CurrentExp.Value -= ExpToLevelUp;
+            ExpToLevelUp += 20;
+            Level.Value += 1;
+        }
+    }
+}
